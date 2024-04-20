ec682cced46befa290eddd3de0e61f64500e62de
rookyere <okyereoseirabbi@gmail.com>
2024-04-19 23:54:28 -0400

    copy of the original master branch

--- finance_manager/settings.py
+++ finance_manager/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-zl#+mgn9y@!_%nxd4@%@pdr)pa34oen46wm@td+@#wg_vwf=m)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
